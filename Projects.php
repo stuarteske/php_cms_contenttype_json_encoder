98f80d10f904a4d848b321806c46cf651c2a0864
stuarteske <sdeske@googlemail.com>
2013-11-25 15:13:00 +0000

    Sub record data now parsed and filter. Update only mode also added.

--- Projects.php
+++ Projects.php
@@ -31,7 +31,13 @@ class Projects {
     /**
      * @var
      */
-    public $projectArray;
+    public $sectionArray;
+
+    /**
+     * @var
+     */
+    private $imageManager;
+
 
     /**
      *
@@ -43,7 +49,10 @@ class Projects {
         );
 
         // Init the project array
-        $this->setProjectArray(array());
+        $this->setSectionArray(array());
+
+        // Setup the image manager
+        $this->setImageManager( new Images_ImageManager() );
     }
 
     /**
@@ -90,45 +99,161 @@ class Projects {
     }
 
     private function parseContentItemsAndTopLevelFields($queryResult) {
-        $projectArray = $this->getProjectArray();
+        $sectionArray = $this->getSectionArray();
 
         while($resultArray = mysql_fetch_array($queryResult)) {
             switch($resultArray[4]) {
+                case 'Intro':
+                    $textString = $resultArray[5];
+
+                    $sectionArray[$resultArray['Slug']][$resultArray[4]] = $this->parseText($textString);
+                    break;
                 case 'Copy':
                     $textString = $resultArray[5];
 
-                    $projectArray[$resultArray['Slug']][$resultArray[4]] = $this->parseText($textString);
-                    //$projectArray[$resultArray['Slug']][$resultArray[4]] = "";
+                    $sectionArray[$resultArray['Slug']][$resultArray[4]] = $this->parseText($textString);
                     break;
                 case 'Image':
                     $imageIdArray = explode( ',', $resultArray[5] );
 
                     foreach( $imageIdArray as $key => $imageId ) {
-                        $projectArray[$resultArray['Slug']]['Image'][$key]['ImageUrl'] = $imageId;
-                        $projectArray[$resultArray['Slug']]['Image'][$key][$resultArray[4]] = '';
+                        $keyValue = '';
+                        $imageData = '';
+
+                        if ( is_numeric($imageId) ) {
+                            $keyValue = $this->getImageManager()->getImageString(
+                                $imageId,
+                                'original'
+                            );
+
+                            $imageFile = file_get_contents($keyValue);
+                            $imageData = 'data:image/png;base64,' . base64_encode($imageFile);
+
+                            $keyValue = 'http://' . $_SERVER['HTTP_HOST'] . str_replace("..", "", "$keyValue");
+
+                            $sectionArray[$resultArray['Slug']]['Image'][$key]['ImageUrl'] = $keyValue;
+                            $sectionArray[$resultArray['Slug']]['Image'][$key][$resultArray[4]] = $imageData;
+                        }
                     }
+                    break;
+                case 'map':
+                    $data = file_get_contents($resultArray[5]);
+                    $imageEncoding = 'data:image/jpeg;base64,' . base64_encode($data);
+
+                    $sectionArray[$resultArray['Slug']][$resultArray[4]] = $imageEncoding;
                     break;
                 default:
                     if (empty($resultArray[5])) $keyValue = 0;
                     else $keyValue = $resultArray[5];
 
-                    $projectArray[$resultArray['Slug']][$resultArray[4]] = $keyValue;
+                    $sectionArray[$resultArray['Slug']][$resultArray[4]] = $keyValue;
                     break;
             }
         }
 
-        $this->setProjectArray($projectArray);
+        $this->setSectionArray($sectionArray);
+    }
+
+    private function parseAndGetLowerLevelFields($sectionArray) {
+
+        foreach(array_keys($sectionArray) as $custompageSlug) {
+            //echo $custompageSlug . '<br />';
+
+            /**
+             * SELECT tcp.ID, tcp.Slug, tcpl.*
+             * FROM tblcustompage AS tcp
+             * JOIN tblcustompagelinks AS tcpl on tcp.ID = tcpl.ParentId
+             * WHERE Slug = 'rbli-'
+             * |------------------------------------------------------|
+             * | Id  | Slug  | linkid | parentid | childid | childkey |
+             * | 199 | rbli- | 1776   | 199      | 6       | Services |
+             * | 199 | rbli- | 1775   | 199      | 195     | Client   |
+             * | 199 | rbli- | 1778   | 199      | 184     | Sectors  |
+             * | 199 | rbli- | 1777   | 199      | 190     | Sectors  |
+             * |------------------------------------------------------|
+             */
+
+            // Get the extra data entities
+            $sql = "SELECT tcp.ID, tcp.Slug, tcpl.*
+              FROM tblcustompage AS tcp
+              JOIN tblcustompagelinks AS tcpl on tcp.ID = tcpl.ParentId
+              WHERE Slug = '" . $custompageSlug . "'";
+
+            $sectionResult = QueryDB($sql);
+            while($sectionResultArray = mysql_fetch_array($sectionResult)) {
+
+                //echo '&nbsp;&nbsp;&nbsp;&nbsp;' . $sectionResultArray['childid'] . ' ' . $sectionResultArray['childkey'] . '<br />' ;
+
+                /**
+                 * SELECT * FROM `tblcustompagedata` WHERE CustomPageID = 111
+                 * |-----------------------------------------------|
+                 * | ID  | CustomPageId | Key     | Value          |
+                 * | 690 | 111          | Title   | Media Planning |
+                 * | 691 | 111          | Image   | 402,497        |
+                 * | 692 | 111          | Online  |                |
+                 * | 693 | 111          | Offline | 1              |
+                 * | 954 | 111          | Copy    | <p>CFA ...     |
+                 * |-----------------------------------------------|
+                 */
+                $queryDataPropertiesSql = "SELECT * "
+                    . "FROM tblcustompagedata "
+                    . "WHERE CustomPageID = " . $sectionResultArray['childid'] . " ";
+
+                $queryDataPropertiesResult = QueryDB($queryDataPropertiesSql);
+
+                $additionalDataBlock = array();
+
+                while($dataPropertiesArray = mysql_fetch_array($queryDataPropertiesResult)) {
+
+                    switch($dataPropertiesArray['Key']) {
+                        case 'Title':
+                            $additionalDataBlock[$dataPropertiesArray['Key']] = $this->parseText($dataPropertiesArray['Value']);
+                            break;
+                        case 'label':
+                            $additionalDataBlock[$dataPropertiesArray['Key']] = $this->parseText($dataPropertiesArray['Value']);
+                            break;
+                        case 'value':
+                            $additionalDataBlock[$dataPropertiesArray['Key']] = $this->parseText($dataPropertiesArray['Value']);
+                            break;
+                        case 'urlId':
+                            $additionalDataBlock[$dataPropertiesArray['Key']] = $this->parseText($dataPropertiesArray['Value']);
+                            break;
+                    }
+                    //echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . $dataPropertiesArray['Key'] . ' ' . $additionalDataBlock[$dataPropertiesArray['Key']] . '<br />' ;
+                }
+
+                $sectionArray[$custompageSlug][$sectionResultArray['childkey']] = array($additionalDataBlock);
+            }
+        }
+
+        return $sectionArray;
     }
 
     /**
      *
      */
-    public function getJson() {
+    public function getJson($updateCheckOnly = false) {
         $queryResult = $this->queryContentItemsAndTopLevelItemDateFields();
-
         $this->parseContentItemsAndTopLevelFields($queryResult);
 
-        echo json_encode($this->getProjectArray());
+        $this->setSectionArray(
+            $this->parseAndGetLowerLevelFields($this->getSectionArray())
+        );
+
+        $outputString = json_encode($this->getSectionArray());
+
+        if ($updateCheckOnly) {
+            $outputArray = array(
+                'version' => hash('sha512', $outputString)
+            );
+        } else {
+            $outputArray = array(
+                'version' => hash('sha512', $outputString),
+                'result' => $this->getSectionArray()
+            );
+        }
+
+        echo json_encode($outputArray);
     }
 
     private function parseImages($imageString = '') {
@@ -163,19 +288,19 @@ class Projects {
 
 
     /**
-     * @param array $projectArray
+     * @param array $sectionArray
      */
-    public function setProjectArray($projectArray)
+    public function setSectionArray($sectionArray)
     {
-        $this->projectArray = $projectArray;
+        $this->sectionArray = $sectionArray;
     }
 
     /**
      * @return array
      */
-    public function getProjectArray()
+    public function getSectionArray()
     {
-        return $this->projectArray;
+        return $this->sectionArray;
     }
 
     /**
@@ -185,149 +310,26 @@ class Projects {
     {
         return $this->protocal;
     }
-}
 
-$projectsConverter = new Projects();
-$projectsConverter->getJson();
-
-// Get the data type name from the php script file
-$dataType = str_replace(".php", "", basename($_SERVER["SCRIPT_NAME"]));
-
-$sql = "SELECT tas.SectionId AS SID, tcp.ID AS CID, tcp.Slug, tcpd.ID AS DID, tcpd.key, tcpd.value "
-    . "FROM tbladminsections AS tas "
-    . " LEFT JOIN tblcustompage AS tcp on tas.SectionId = tcp.Section "
-    . " LEFT JOIN tblcustompagedata AS tcpd on tcp.ID = tcpd.CustomPageID "
-    . "WHERE tas.SectionName = '" . $dataType . "' "
-    . "AND tcp.Enabled = 1 ";
-
-$queryResult = QueryDB($sql);
-
-$imageManager = new Images_ImageManager();
-
-$projectArray = array();
-
-while($resultArray = mysql_fetch_array($queryResult)) {
-    switch($resultArray[4]) {
-        case 'Copy':
-            $keyValue = preg_replace('/\r|\n|\t/m','', $resultArray[5]);
-            $keyValue = htmlentities($keyValue);
-
-            $projectArray[$resultArray['Slug']][$resultArray[4]] = $keyValue;
-            //$projectArray[$resultArray['Slug']][$resultArray[4]] = "";
-            break;
-        case 'Image':
-            $imageIdArray = explode( ',', $resultArray[5] );
-
-            foreach( $imageIdArray as $key => $imageId ) {
-                $projectArray[$resultArray['Slug']]['Image'][$key]['ImageUrl'] = $imageId;
-                $projectArray[$resultArray['Slug']]['Image'][$key][$resultArray[4]] = '';
-            }
-
-//            $keyValue = $imageManager->getImageString(
-//                $resultArray[5],
-//                'portfolio-main'
-//            );
-
-            //$imageFile = file_get_contents($keyValue);
-            //$imageData = 'data:image/png;base64,' . base64_encode($imageFile);
-
-//            $projectArray[$resultArray['Slug']]['ImageUrl'] = $keyValue;
-//            $projectArray[$resultArray['Slug']][$resultArray[4]] = '';
-            break;
-        default:
-            if (empty($resultArray[5])) $keyValue = 0;
-                else $keyValue = $resultArray[5];
-
-            $projectArray[$resultArray['Slug']][$resultArray[4]] = $keyValue;
-            break;
-    }
-}
-
-foreach(array_keys($projectArray) as $custompageSlug) {
-//    echo $custompageSlug . '<br />';
-
-    /*
-    SELECT tcp.ID, tcp.Slug, tcpl.*
-    FROM tblcustompage AS tcp
-    JOIN tblcustompagelinks AS tcpl on tcp.ID = tcpl.ParentId
-    WHERE Slug = 'rbli-'
-    |------------------------------------------------------|
-    | Id  | Slug  | linkid | parentid | childid | childkey |
-    | 199 | rbli- | 1776   | 199      | 6       | Services |
-    | 199 | rbli- | 1775   | 199      | 195     | Client   |
-    | 199 | rbli- | 1778   | 199      | 184     | Sectors  |
-    | 199 | rbli- | 1777   | 199      | 190     | Sectors  |
-    |------------------------------------------------------|
+    /**
+     * @param mixed $imageManager
      */
+    public function setImageManager($imageManager)
+    {
+        $this->imageManager = $imageManager;
+    }
 
-    // Get the extra data entities
-    $sql = "SELECT tcp.ID, tcp.Slug, tcpl.*
-      FROM tblcustompage AS tcp
-      JOIN tblcustompagelinks AS tcpl on tcp.ID = tcpl.ParentId
-      WHERE Slug = '" . $custompageSlug . "'";
-
-    $sectionResult = QueryDB($sql);
-    while($sectionResultArray = mysql_fetch_array($sectionResult)) {
-//        echo '&nbsp;&nbsp;&nbsp;&nbsp;' . $resultArray['childid'] . ' ' . $resultArray['childkey'] . '<br />' ;
-        //$projectArray[$resultArray['Slug']][$resultArray['childkey']]
-
-        /*
-         SELECT * FROM `tblcustompagedata` WHERE CustomPageID = 111
-        |-----------------------------------------------|
-        | ID  | CustomPageId | Key     | Value          |
-        | 690 | 111          | Title   | Media Planning |
-        | 691 | 111          | Image   | 402,497        |
-        | 692 | 111          | Online  |                |
-        | 693 | 111          | Offline | 1              |
-        | 954 | 111          | Copy    | <p>CFA ...     |
-        |-----------------------------------------------|
-         */
-        $queryDataPropertiesSql = "SELECT * "
-            . "FROM tblcustompagedata "
-            . "WHERE CustomPageID = " . $sectionResultArray['childid'] . " ";
-
-        $queryDataPropertiesResult = QueryDB($queryDataPropertiesSql);
-
-        $additionalDataBlock = array();
-
-        while($dataPropertiesArray = mysql_fetch_array($queryDataPropertiesResult)) {
-
-//            echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
-//                . $dataPropertiesArray['Key']
-//                . ' '
-//                . $dataPropertiesArray['Value']
-//                . '<br />' ;
-
-            switch($dataPropertiesArray['Key']) {
-                case 'Image':
-                    $imageIdArray = explode( ',', $dataPropertiesArray['Value'] );
-
-                    foreach( $imageIdArray as $key => $imageId ) {
-                        $additionalDataBlock[$dataPropertiesArray['Key']][$key]['ImageUrl'] = $imageId;
-                        $additionalDataBlock[$dataPropertiesArray['Key']][$key]['Image'] = '';
-                    }
-                    break;
-                case 'Copy':
-                    $keyValue = preg_replace('/\r|\n|\t/m','', $dataPropertiesArray['Value']);
-                    $keyValue = htmlentities($keyValue);
-
-                    $additionalDataBlock[$dataPropertiesArray['Key']] = $keyValue;
-                    //$additionalDataBlock[$dataPropertiesArray['Key']] = "";
-                    break;
-                default:
-                    if (empty($dataPropertiesArray['Value'])) $keyValue = 0;
-                        else $keyValue = $dataPropertiesArray['Value'];
-
-                    $additionalDataBlock[$dataPropertiesArray['Key']] = $keyValue;
-                    break;
-            }
-        }
-
-        $projectArray[$custompageSlug][$sectionResultArray['childkey']][] = $additionalDataBlock;
+    /**
+     * @return mixed
+     */
+    public function getImageManager()
+    {
+        return $this->imageManager;
     }
 }
 
-//echo var_dump(array_keys($projectArray));
- //echo var_dump($projectArray[$resultArray['Slug']]);
+$projectEncoder = new Projects();
 
-//echo json_encode($projectArray);
+if ( isset($_GET['update']) ) $projectEncoder->getJson(true);
+    else $projectEncoder->getJson();
+
